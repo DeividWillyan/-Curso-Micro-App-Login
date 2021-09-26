6a31ffeda3eca075ad1728ea6201bdb7d870496a
Deivid Willyan <deiviid_@hotmail.com>
2021-09-26 17:22:43 -0300

    Add - Dart-Defines / Flavors

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -3,6 +3,10 @@ import 'package:micro_commons_design_system/micro_commons_design_system.dart';
 import 'package:micro_core/micro_core.dart';
 
 class LoginPage extends StatelessWidget {
+  String _appName = const String.fromEnvironment('DEFINE_APP_NAME');
+  String _suffixName = const String.fromEnvironment('DEFINE_SUFFIX_NAME');
+  String _server = const String.fromEnvironment('DEFINE_SERVER');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +14,16 @@ class LoginPage extends StatelessWidget {
         title: Text('Login'),
       ),
       body: Center(
-        child: CustomButtonWidget(
-          text: 'goToHome',
-          onPressed: () => navigatorKey.currentState?.pushNamed('/home'),
+        child: Column(
+          children: [
+            Text(_appName, style: TextStyle(fontSize: 20)),
+            Text(_suffixName, style: TextStyle(fontSize: 20)),
+            Text(_server, style: TextStyle(fontSize: 20)),
+            CustomButtonWidget(
+              text: 'goToHome',
+              onPressed: () => navigatorKey.currentState?.pushNamed('/home'),
+            ),
+          ],
         ),
       ),
     );
